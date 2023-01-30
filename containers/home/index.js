6b16eed755148f34539830e87d4ad34cbd0cf24e
Muhammet-Developer <muhammed_249@outlook.com>
2023-01-30 22:56:08 +0300

    first commit

---
+++ containers/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Movies from "@/mocks/movies.json"
+import Genres from "@/mocks/genres.json"
+import FeatureMovie from "@/components/featured-movie"
+import Categories from '@/components/categories'
+const HomePage = () => {
+  console.log(Movies)
+  return (
+    <div>
+      <FeatureMovie movie={Movies.results[0]} />
+      <Categories categories={Genres.genres.slice(0,5)}/>
+    </div>
+  )
+}
+
+export default HomePage
